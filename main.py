8606190d1d05658963e47f6671cd710f09249860
Tihomir Stanoev <tstanoev@yandex.com>
2025-03-24 20:22:13 +0200

    Update main menu

--- main.py
+++ main.py
@@ -1,24 +1,29 @@
-import re
-from validators import PasswordValidator
+from validators import PasswordValidator, EmailValidator
 
+
+def validator_result(string, validator_object):
+    is_valid = validator_object.check_result(string)
+    return is_valid
 
 
 def main():
-
     password_validator = PasswordValidator()
+    email_validator = EmailValidator()
 
+    print('Validate & Extract Data with Python REGEX!')
     while True:
 
         print('1. Password validator')
+        print('2. Email validator')
+
         choose = input('Your choose: ')
         if choose == '1':
             password = input('Your password:')
-            is_valid = password_validator.check_password(password)
-            print(is_valid)
+            print(validator_result(password, password_validator))
 
-
-
-
+        elif choose == '2':
+            email = input('Your email: ')
+            print(validator_result(email, email_validator))
 
 
 if __name__ == "__main__":
