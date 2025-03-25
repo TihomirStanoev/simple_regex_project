c2e33c1c5ae006bac3f9ad46110dd210db3e380d
Tihomir Stanoev <tstanoev@yandex.com>
2025-03-26 00:09:06 +0200

    Create class Extractor

--- manipulate_text.py
+++ manipulate_text.py
@@ -1,1 +1,25 @@
 from regex import RegEx
+
+
+class Extractor:
+    """
+    A class for extracting email addresses and phone numbers from a given text.
+    """
+    def __init__(self, text=''):
+        self.text = text
+
+    def extract_phone_and_email(self):
+        extracted_emails = self.extract_email()
+        extracted_phones = self.extract_phone()
+        return extracted_emails, extracted_phones
+
+    def extract_email(self) -> list:
+        emails = RegEx.extract_email.findall(self.text)
+        return [email[0] for email in emails]
+
+    def extract_phone(self) -> list:
+        phones = RegEx.extract_phone.findall(self.text)
+        return [phone[0] for phone in phones]
+
+
+
