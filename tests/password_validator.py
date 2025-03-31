69ea4338f757352c7b2d73c238beda0f996393a9
Tihomir Stanoev <tstanoev@yandex.com>
2025-03-31 20:49:54 +0200

    PassowrdValidator tests

--- tests/password_validator.py
+++ tests/password_validator.py
@@ -10,30 +10,33 @@ class TestPasswordValidator(unittest.TestCase):
 
     def test_short_length_of_password(self):
         self.assertFalse(self.password_validator.check_result('000000'))
-        self.assertFalse(self.password_validator.check_result('A#0000'))
-        self.assertFalse(self.password_validator.check_result('aA#000'))
+        self.assertFalse(self.password_validator.check_result('A!0000'))
+        self.assertFalse(self.password_validator.check_result('aA!000'))
         self.assertFalse(self.password_validator.check_result('aaaaaa'))
         self.assertFalse(self.password_validator.check_result('@@@@@@'))
         self.assertFalse(self.password_validator.check_result('11@aA11'))
-        self.assertTrue(self.password_validator.check_result(self.valid_password))
 
 
     def test_uppercase_exist(self):
-        pass
+        self.assertFalse(self.password_validator.check_result('aaaaaaaaa'))
+        self.assertFalse(self.password_validator.check_result('aaaaaa12a'))
+        self.assertFalse(self.password_validator.check_result('aa!aa'))
+        self.assertFalse(self.password_validator.check_result('aa!aaa12a'))
 
     def test_digit_exist(self):
-        pass
+        self.assertFalse(self.password_validator.check_result('aaaaaaaaa'))
+        self.assertFalse(self.password_validator.check_result('AAaaaaaaa'))
+        self.assertFalse(self.password_validator.check_result('!!aaaaaaa'))
+        self.assertFalse(self.password_validator.check_result('!!aaaA4'))
+
 
     def test_special_exist(self):
-        pass
+        self.assertFalse(self.password_validator.check_result('AAa12aaaa'))
+        self.assertFalse(self.password_validator.check_result('AAaaa1aa'))
+
 
     def test_valid_password(self):
-        pass
-
-
-
-
-
+        self.assertTrue(self.password_validator.check_result(self.valid_password))
 
 
 if __name__ == "__main__":
